7e91073effc16ee4b781071090d953e583cd1a51
Alex <araustincs@gmail.com>
2025-12-18 22:53:12 -0500

    Removed forge, neoforge support. Consolidated modules into single fabric module.

--- fabric/src/main/java/io/github/maxencedc/sparsestructures/command/DumpStructureSetsCommand.java
+++ fabric/src/main/java/io/github/maxencedc/sparsestructures/command/DumpStructureSetsCommand.java
@@ -3,9 +3,9 @@ package io.github.maxencedc.sparsestructures.command;
 import com.mojang.brigadier.CommandDispatcher;
 import io.github.maxencedc.sparsestructures.Constants;
 import io.github.maxencedc.sparsestructures.StructureSetsSet;
-import net.minecraft.ChatFormatting;
-import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.network.chat.Component;
+import net.minecraft.util.Formatting;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -14,33 +14,33 @@ import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static net.minecraft.commands.Commands.literal;
+import static net.minecraft.server.command.CommandManager.literal;
 
 
 public class DumpStructureSetsCommand {
 
-    public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
 
         dispatcher.register(
             literal("dumpstructuresets")
-                .requires(cs -> cs.hasPermission(2))
+                .requires(cs -> cs.hasPermissionLevel(2))
                 .executes(context -> {
-                    context.getSource().sendSuccess(() -> Component.translatable("command.sparsestructures.dump.dumping"), false);
+                    context.getSource().sendFeedback(() -> Text.translatable("command.sparsestructures.dump.dumping"), false);
                     String fileName = new SimpleDateFormat("'structure_sets_dump_'yy_MM_dd_HH_mm'.txt'").format(new Date());
                     try {
                         dumpStructureSets(fileName);
-                        context.getSource().sendSuccess(() -> {
-                            boolean isDedicatedServer = context.getSource().getServer().isDedicatedServer();
-                            Component underlinedFile = Component.literal(fileName).withStyle(ChatFormatting.UNDERLINE);
-                            String message = Component.translatable("command.sparsestructures.dump.success", underlinedFile).getString();
+                        context.getSource().sendFeedback(() -> {
+                            boolean isDedicatedServer = context.getSource().getServer().isDedicated();
+                            Text underlinedFile = Text.literal(fileName).formatted(Formatting.UNDERLINE);
+                            String message = Text.translatable("command.sparsestructures.dump.success", underlinedFile).getString();
                             if (isDedicatedServer) {
-                                message += "\n" + Component.translatable("command.sparsestructures.dump.server").getString();
+                                message += "\n" + Text.translatable("command.sparsestructures.dump.server").getString();
                             }
-                            return Component.literal(message);
+                            return Text.literal(message);
                         }, false);
                         return 1;
                     } catch (IOException e) {
-                        context.getSource().sendFailure(Component.translatable("command.sparsestructures.dump.failure"));
+                        context.getSource().sendError(Text.translatable("command.sparsestructures.dump.failure"));
                         Constants.LOG.error("Failed to dump structure sets\n", e);
                         return 0;
                     }
